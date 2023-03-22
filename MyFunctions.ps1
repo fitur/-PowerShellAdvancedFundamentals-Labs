04ec1615ec8380b77f8c3ae700a425e2c44aeda6
Peter Olausson <peter.olausson@gmail.com>
2023-03-22 09:58:27 +0100

    changed GetUserData to use mandatory parameter for file input

--- MyFunctions.ps1
+++ MyFunctions.ps1
@@ -1,12 +1,16 @@
 function GetUserData {
+    [CmdletBinding()]
     param (
+        [Parameter(mandatory=$true)]
         [System.IO.FileInfo]$InputFile
     )
-    $MyUserListFile = "$PSScriptRoot\MyLabFile.csv"
-    $CSV = Import-Csv -Path $MyUserListFile
+    #$MyUserListFile = "$PSScriptRoot\MyLabFile.csv"
+    #$CSV = Import-Csv -Path $MyUserListFile
+    $CSV = Import-Csv -Path $InputFile
     return $CSV
 }
 
 function Get-CourseUser {
     
 }
+
