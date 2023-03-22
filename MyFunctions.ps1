42e3a40702bad1659baad2fc7c6774ae955ba6af
Peter Olausson <peter.olausson@gmail.com>
2023-03-22 09:53:48 +0100

    added function Get-CourseUser

--- MyFunctions.ps1
+++ MyFunctions.ps1
@@ -1,5 +1,12 @@
 function GetUserData {
+    param (
+        [System.IO.FileInfo]$InputFile
+    )
     $MyUserListFile = "$PSScriptRoot\MyLabFile.csv"
     $CSV = Import-Csv -Path $MyUserListFile
     return $CSV
+}
+
+function Get-CourseUser {
+    
 }
