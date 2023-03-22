b6e37efc4e30a870de5972f8581d0d06d52a44ba
Peter Olausson <peter.olausson@gmail.com>
2023-03-22 10:30:44 +0100

    added new functions

--- MyFunctions.ps1
+++ MyFunctions.ps1
@@ -11,6 +11,40 @@ function GetUserData {
 }
 
 function Get-CourseUser {
+    [CmdletBinding()]
+    param (
+        [Parameter(mandatory=$false)]
+        [String]$Name,
+        [Parameter(mandatory=$false)]
+        [int]$OlderThan
+    )
     
-}
+    begin {
+        # Get user data from helper function
+        $UserData = GetUserData -InputFile "C:\Git\-PowerShellAdvancedFundamentals-Labs\MyLabFile.csv"
+    }
+    
+    process {
+        # If a name is given, return only specific user
+        if (![String]::IsNullOrEmpty($Name)) {
+            Write-Host "Name to look for: $Name"
+            Write-Host "List size $($UserData.Count)"
+            $UserData = $UserData | Where-Object {$_.Name -match $Name}
+        } else {
+            $UserData = $UserData
+        }
 
+        # If an age is given, return only older than that value
+        if ($null -ne $OlderThan) {
+            Write-Host "Age to look for: $OlderThan"
+            $UserData = $UserData | Where-Object {$_.Age -gt $OlderThan}
+        } else {
+            $UserData = $UserData
+        }
+    }
+    
+    end {
+        # Return data
+        return $UserData
+    }
+}
