ce8c1079996cbaf1e7af8b8a7e9a496c994e9bdd
Peter Olausson <peter.olausson@gmail.com>
2023-03-22 14:31:01 +0100

    new function Remove-CourseUser

--- MyFunctions.ps1
+++ MyFunctions.ps1
@@ -4,50 +4,24 @@ function GetUserData {
         [Parameter(mandatory=$false)]
         [System.IO.FileInfo]$MyUserListFile = "$PSScriptRoot\MyLabFile.csv"
     )
-    #$MyUserListFile = "$PSScriptRoot\MyLabFile.csv"
-    #$CSV = Import-Csv -Path $MyUserListFile
-    $CSV = Import-Csv -Path $MyUserListFile
-    return $CSV
+    $MyUserList = Get-Content -Path $MyUserListFile | ConvertFrom-Csv
+    return $MyUserList
 }
 
 function Get-CourseUser {
     [CmdletBinding()]
     param (
-        [Parameter(mandatory=$false)]
-        [String]$Name,
+        [Parameter(mandatory=$false, HelpMessage="Specify a first and last name.")]
+        [ValidateNotNullOrEmpty()]
+        [string]$Name,
 
-        [Parameter(mandatory=$false)]
-        [int]$OlderThan
+        [Parameter(mandatory=$false, HelpMessage="Specify an age in digits.")]
+        [ValidateNotNullOrEmpty()]
+        [int]$OlderThan = 65
     )
     
-    begin {
-        # Get user data from helper function
-        $UserData = GetUserData
-    }
-    
-    process {
-        # If a name is given, return only specific user
-        if (![String]::IsNullOrEmpty($Name)) {
-            Write-Host "Name to look for: $Name"
-            Write-Host "List size $($UserData.Count)"
-            $UserData = $UserData | Where-Object {$_.Name -match $Name}
-        } else {
-            $UserData = $UserData
-        }
-
-        # If an age is given, return only older than that value
-        if ($null -ne $OlderThan) {
-            Write-Host "Age to look for: $OlderThan"
-            $UserData = $UserData | Where-Object {$_.Age -gt $OlderThan}
-        } else {
-            $UserData = $UserData
-        }
-    }
-    
-    end {
-        # Return data
-        return $UserData
-    }
+    # When Name might be unspecified, Where-Object matches all, else only specified
+    return GetUserData | Where-Object {($_.Name -match $Name) -and ($_.Age -gt $OlderThan)}
 }
 
 function Add-CourseUser {
@@ -55,7 +29,7 @@ function Add-CourseUser {
     param (
         [Parameter(mandatory=$false, HelpMessage="Specify a file.")]
         [ValidateNotNullOrEmpty()]
-        [System.IO.FileInfo]$DatabaseFile = "$PSScriptRoot\MyLabFile.csv",
+        [System.IO.FileInfo]$MyUserListFile = "$PSScriptRoot\MyLabFile.csv",
 
         [Parameter(mandatory=$true, HelpMessage="Specify a first and last name.")]
         [ValidateNotNullOrEmpty()]
@@ -74,16 +48,75 @@ function Add-CourseUser {
         [string]$UserID = (Get-Random -Minimum 10 -Maximum 100000)
     )
     
-    # Read file content if exist else initialize
-    if (-not (Test-Path -Path $DatabaseFile)) {
-        Set-Content -Value "Name,Age,Color,Id" -Path $DatabaseFile
+    # Create file if not exist
+    if (-not (Test-Path -Path $MyUserListFile)) {
+        Set-Content -Value "Name,Age,Color,Id" -Path $MyUserListFile
     }
-    $NewCSv = Get-Content $DatabaseFile -Raw
+    
+    # Read file content
+    $NewCSv = Get-Content $MyUserListFile -Raw
 
     # Add input to variable
     $MyCsvUser = "$Name,$Age,{0},{1}" -f $Color, $UserID
 
-    # Add variable to 
+    # Add variable to user list
     $NewCSv += $MyCsvUser
-    Set-Content -Value $NewCSv -Path $DatabaseFile
+
+    # Save user
+    Set-Content -Value $NewCSv -Path $MyUserListFile
+}
+
+function Remove-CourseUser {
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact='High')]
+    param (
+        [Parameter(mandatory=$false, HelpMessage="Specify a file.")]
+        [ValidateNotNullOrEmpty()]
+        [System.IO.FileInfo]$MyUserListFile = "$PSScriptRoot\MyLabFile2.csv"
+    )
+
+    Begin {
+        if (-not $PSBoundParameters.ContainsKey('Confirm')) {
+            $ConfirmPreference = $PSCmdlet.SessionState.PSVariable.GetValue('ConfirmPreference')
+        }
+        if (-not $PSBoundParameters.ContainsKey('WhatIf')) {
+            $WhatIfPreference = $PSCmdlet.SessionState.PSVariable.GetValue('WhatIfPreference')
+        }
+    }
+
+    Process {
+        $MyUserList = Get-Content -Path $MyUserListFile | ConvertFrom-Csv
+        $RemoveUser = $MyUserList | Out-GridView -PassThru
+        $MyUserList = $MyUserList | Where-Object {
+            -not (
+                $_.Name -eq $RemoveUser.Name -and
+                $_.Age -eq $RemoveUser.Age -and
+                $_.Color -eq $RemoveUser.Color -and
+                $_.Id -eq $RemoveUser.Id
+            )
+        }
+
+        # Execute change if based on confirmation and impact
+        if ($PSCmdlet.ShouldProcess([string]$RemoveUser.Name)) {
+            Set-Content -Value $MyUserList -Path $MyUserListFile
+        } else {
+            Write-Host "Did not remove user $($RemoveUser.Name)"
+        }
+    }
+}
+
+function Test-ShouldProcess {
+    [CmdletBinding(
+        SupportsShouldProcess,
+        ConfirmImpact = 'High'
+    )]
+    param()
+
+    Write-Host "testing"
+
+    if ($Force -or $PSCmdlet.ShouldProcess("ShouldProcess?")) {
+        Write-Verbose ('[{0}] Reached command' -f $MyInvocation.MyCommand)
+        # Variable scope ensures that parent session remains unchanged
+        $ConfirmPreference = 'None'
+        Write-Output "Some Action"
+    }
 }
